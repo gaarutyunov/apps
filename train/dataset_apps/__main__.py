2335cbe6fbc6d75b3efa9d5cf0f99702074d4648
German Arutyunov <garutyunov@tenderhub.ru>
2022-05-27 21:39:05 +0300

    module fix

---
+++ train/dataset_apps/__main__.py
@@ -0,0 +1,31 @@
+import os
+import json
+
+import transformers
+
+from APPSBaseDataset import APPSBaseDataset
+
+if __name__ == '__main__':
+    # Do sanity checking
+    with open(os.path.expanduser("~/apps/data_split/train.json")) as f:
+        fnames = json.load(f)
+
+    model = transformers.GPTNeoXForCausalLM.from_pretrained("EleutherAI/gpt-neox-20b")
+    tokenizer = transformers.GPTNeoXTokenizerFast.from_pretrained("EleutherAI/gpt-neox-20b")
+    dataset = APPSBaseDataset(
+        dataroot='~/apps/',
+        problem_dirs=fnames,
+        mode="EleutherAI/gpt-neox-20b",
+        max_tokens=2048,
+        sample_mode='uniform_prob'
+    )
+
+    e = dataset[0]
+    print(e)
+    print("------- input_ids ------------------------------------------------------------------------------------")
+    print(tokenizer.decode(e['input_ids']))
+    print("------- labels ------------------------------------------------------------------------------------")
+    labels = e['labels']
+    labels[labels == -100] = tokenizer.eos_token_id
+    labels_str = tokenizer.decode(labels)
+    print(labels_str)
