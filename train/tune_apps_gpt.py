9838d190e009c8deb1d999d9e064aaef2f15506f
German Arutyunov <garutyunov@tenderhub.ru>
2022-09-12 18:25:07 +0300

    fix merge

--- train/tune_apps_gpt.py
+++ train/tune_apps_gpt.py
@@ -14,44 +14,34 @@ import torch.multiprocessing
 
 torch.multiprocessing.set_sharing_strategy("file_system")
 
-
 transformers.logging.set_verbosity_debug()
 os.environ["NCCL_DEBUG"] = "INFO"
 os.environ["MASTER_PORT"] = "19994"
 
 
 def run_training(args, train_data):
-
     ## Checkpoint Loading ########################################################
     if args.load:
-<<<<<<< HEAD
         if 'neox' in args.load:
             model = transformers.GPTNeoXForCausalLM.from_pretrained(args.load).half().cuda()
+        elif 'gpt-j' in args.load:
+            model = transformers.GPTJForCausalLM.from_pretrained(args.load)
         elif '2700' in args.load:
-=======
-        if "neox" in args.load:
-            model = transformers.GPTNeoXForCausalLM.from_pretrained(args.load)
-        elif "2700" in args.load:
->>>>>>> 96109da3546bb99bf82cfc87d6bb575b524a500a
             model = transformers.GPTNeoForCausalLM.from_pretrained(args.load)
         else:
             model = transformers.GPT2LMHeadModel.from_pretrained(args.load)
         print(f"Loaded model from {args.load}")
     else:
-<<<<<<< HEAD
         if 'neox' in args.arch:
             model = transformers.GPTNeoXForCausalLM.from_pretrained(args.arch).half().cuda()
-=======
-        if "neox" in args.arch:
-            model = transformers.GPTNeoXForCausalLM.from_pretrained(args.arch)
->>>>>>> 96109da3546bb99bf82cfc87d6bb575b524a500a
+        elif 'gpt-j' in args.load:
+            model = transformers.GPTJForCausalLM.from_pretrained(args.load)
         elif "EleutherAI" in args.arch:
             model = transformers.GPTNeoForCausalLM.from_pretrained(args.arch)
         else:
             model = transformers.GPT2LMHeadModel.from_pretrained(args.arch)
 
     if args.resume:
-        raise NotImplementedError
         model = transformers.GPT2LMHeadModel.from_pretrained(args.resume)
         print(f"Loaded model from {args.resume}")
         start_epoch = 0
@@ -77,11 +67,7 @@ def run_training(args, train_data):
         num_train_epochs=args.epochs,
         per_device_train_batch_size=args.batch_size_per_replica,
         gradient_accumulation_steps=args.grad_acc_steps,
-<<<<<<< HEAD
-	gradient_checkpointing=True,
-
-=======
->>>>>>> 96109da3546bb99bf82cfc87d6bb575b524a500a
+        gradient_checkpointing=True,
         learning_rate=args.lr,
         weight_decay=0.05,
         # warmup_steps=args.lr_warmup_steps,
@@ -113,32 +99,20 @@ def run_training(args, train_data):
 
 
 def get_dataset(args):
-
     fnames = os.listdir(args.apps_train_files)
-<<<<<<< HEAD
     mode = args.load if args.load is not None else args.arch
-=======
-
->>>>>>> 96109da3546bb99bf82cfc87d6bb575b524a500a
     train_data = APPSBaseDataset(
         dataroot=args.apps_dataroot,
         problem_dirs=fnames,
-<<<<<<< HEAD
-        mode=mode, 
+        mode=mode,
         max_tokens=2048 if ('EleutherAI' in args.arch or '2700' in args.load) else 1024,
         sample_mode=args.apps_sample_mode
-=======
-        mode=args.arch,
-        max_tokens=2048 if ("EleutherAI" in args.arch or "2700" in args.load) else 1024,
-        sample_mode=args.apps_sample_mode,
->>>>>>> 96109da3546bb99bf82cfc87d6bb575b524a500a
     )
 
     return train_data
 
 
 def main(args):
-
     argsdict = vars(args)
     print(pprint.pformat(argsdict))
 
@@ -157,55 +131,28 @@ if __name__ == "__main__":
     import argparse
 
     parser = argparse.ArgumentParser(description="Language Modelling on Code")
-<<<<<<< HEAD
-    parser.add_argument('--arch', default='EleutherAI/gpt-neox-20b', choices=transformers.GPT2_PRETRAINED_MODEL_ARCHIVE_LIST + ["EleutherAI/gpt-neo-2.7B", "EleutherAI/gpt-neox-20b"])
+    parser.add_argument('--arch', default='EleutherAI/gpt-j-6B',
+                        choices=transformers.GPT2_PRETRAINED_MODEL_ARCHIVE_LIST + ["EleutherAI/gpt-neo-2.7B",
+                                                                                   "EleutherAI/gpt-neox-20b",
+                                                                                   "EleutherAI/gpt-j-6B"])
     parser.add_argument('--dummy-model', action='store_true')
-    parser.add_argument('--load', default='~/apps/gpt-neox-20b', type=os.path.expanduser)
+    parser.add_argument('--load', default='~/apps/gpt-j-6B', type=os.path.expanduser)
     parser.add_argument('--resume', default=None, type=str)
 
     # Dataloading
     parser.add_argument('--apps-dataroot', default='~/apps/APPS/train', type=os.path.expanduser)
     parser.add_argument('--apps-train-files', default='~/apps/APPS/train', type=os.path.expanduser)
     parser.add_argument('--apps-sample-mode', default='uniform_sol')
-    
-=======
-    parser.add_argument(
-        "--arch",
-        default="EleutherAI/gpt-neox-20b",
-        choices=transformers.GPT2_PRETRAINED_MODEL_ARCHIVE_LIST
-        + ["EleutherAI/gpt-neo-2.7B", "EleutherAI/gpt-neox-20b"],
-    )
-    parser.add_argument("--dummy-model", action="store_true")
-    parser.add_argument("--load", default=None, type=str)
-    parser.add_argument("--resume", default=None, type=str)
 
-    # Dataloading
-    parser.add_argument("--apps-dataroot", default="~/apps/", type=os.path.expanduser)
-    parser.add_argument(
-        "--apps-train-files",
-        default="~/apps/data_split/train.json",
-        type=os.path.expanduser,
-    )
-    parser.add_argument("--apps-sample-mode", default="uniform_sol")
-
->>>>>>> 96109da3546bb99bf82cfc87d6bb575b524a500a
     # Training
     parser.add_argument("--epochs", default=10, type=int)
     parser.add_argument("--lr", default=5e-5, type=float)
     # parser.add_argument('--lr-warmup-steps', default=500, type=int)
-<<<<<<< HEAD
     parser.add_argument('--batch-size-per-replica', default=1, type=int)
     parser.add_argument('--grad-acc-steps', default=4, type=int)
     parser.add_argument('--local_rank', default=-1, type=int)
     parser.add_argument('--deepspeed', default="~/apps/train/deepspeed_config.json", type=os.path.expanduser)
     parser.add_argument('--fp16', default=True, action='store_true')
-=======
-    parser.add_argument("--batch-size-per-replica", default=8, type=int)
-    parser.add_argument("--grad-acc-steps", default=8, type=int)
-    parser.add_argument("--local_rank", default=0, type=int)
-    parser.add_argument("--deepspeed", default="deepspeed_config.json", type=str)
-    parser.add_argument("--fp16", default=True, action="store_true")
->>>>>>> 96109da3546bb99bf82cfc87d6bb575b524a500a
 
     # Logging and stuff
     parser.add_argument(
